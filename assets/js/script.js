e51e331795d5bbbf7dc6084b2b80d5b60bc6ee1f
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-03-15 13:51:48 -0300

    Projeto responsivo para mobile e desktop! Lógica criada e todas as funcionalidades feitas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,37 @@
 
+; (function () {
+    'use strict'
 
-let section = document.querySelector("section");
-section.setAttribute("id", "mainContent")
+    const button = document.querySelector("#calcular");
+
+    button.addEventListener("click", () => {
+
+        let num = document.getElementById("num");
+        let end = document.getElementById("end");
+
+
+        if (num.value == 0 || end.value == 0) {
+            alert('Preencha todos os campos');
+        } else {
+
+            let nConvertido = parseInt(num.value);
+            let eConvertido = parseInt(end.value);
+
+            let x = 0;
+            let resposta = '';
+            let res = document.getElementById('resposta');
+            res.innerHTML = `A tabuada do ${nConvertido} é: <br>`
+            res.style.textAlign = 'center'
+
+            do { // Faça isso
+                resposta = nConvertido * x;
+                res.innerHTML += `${x} * ${nConvertido} = ${resposta} <br>`
+                x++
+            } while (x <= eConvertido); // Enquanto isso for verdade
+        }
+    });
+
+
+
+
+})();
